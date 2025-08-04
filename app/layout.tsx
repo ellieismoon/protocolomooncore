dd16b98f754cd513408373c9ace2b1fe9dece41b
ellieismoon <szarewr0987@gmail.com>
2025-08-04 11:47:39 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Protocolo Mooncore',
+  description: 'Pagina para el tabletop del mismo nombre',
+  generator: 'Ellieismoon',
 }
 
 export default function RootLayout({
